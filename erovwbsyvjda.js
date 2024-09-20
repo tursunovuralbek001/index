9af975cd0c8d2129ed432acee510ec6eb95741dc
tursunovuralbek001 <tursunovuralbek001@gmail.com>
2024-09-20 23:53:21 +0500

    update erovwbsyvjda.js

--- erovwbsyvjda.js
+++ erovwbsyvjda.js
@@ -1,7 +1,7 @@
+
 document.getElementById("aaab").innerHTML = "Bismillahir rohmanir rohiym";
 document.getElementById("aaac").innerHTML = "QUR’ONI KARIM";
 document.getElementById("aaad").innerHTML = "Ma’nolari tarjimasi";
-document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
